6e85e75a60ae57a786d2cfcdeb675aff773c73a3
Paulo Gonçalves <paulojsgg@gmail.com>
2021-02-16 09:02:31 +0000

    Updates

---
+++ precache-manifest.d49d06d7040812e5484580db8a77cf0c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e79764c7f771768457715875d77665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d66ae22316c3a0667f",
+    "url": "/static/css/2.797969c2.chunk.css"
+  },
+  {
+    "revision": "2bd87010049a7c8b6207",
+    "url": "/static/css/main.86dcf04b.chunk.css"
+  },
+  {
+    "revision": "d3d66ae22316c3a0667f",
+    "url": "/static/js/2.9d31f876.chunk.js"
+  },
+  {
+    "revision": "2bd87010049a7c8b6207",
+    "url": "/static/js/main.51e17f28.chunk.js"
+  },
+  {
+    "revision": "2caa8c0ba7d7bc1dc221",
+    "url": "/static/js/runtime~main.f3cbd3f8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "380edcdccf81562270936898cce99b62",
+    "url": "/static/media/slide-2.380edcdc.jpg"
+  }
+]);
